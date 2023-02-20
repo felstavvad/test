730a038332a3c035be42c3844b036f2de029ae6c
robscurity <robscurity@icloud.com>
2023-02-20 17:23:45 +0100

    random
    random

--- example.py
+++ example.py
@@ -1,1 +1,23 @@
-print("Hello World")
+import re
+from random import choice
+
+
+def generate_password():
+    global passwd
+    global match
+    lst = [(33, 91), (97, 127)]
+    collect_chr = [chr(k) for i, j in lst for k in range(i, j)]
+    passwd = ''. join([choice(collect_chr) for l in range(1, 9)])
+    rgx = ['\W', '[a-z]', '[A-Z]', '\d']
+    match = list(filter(lambda x: re.search(r''+x+'', passwd), rgx))
+
+
+generate_password()
+
+
+while True:
+    if len(match) == 4:
+        print(passwd)
+        break
+else:
+    generate_password()
